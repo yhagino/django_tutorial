b682f175ddaf77f2d8af7a497c6d7d874c87c5fa
YukiarC <yuukihagino@gmail.com>
2020-12-28 13:40:31 +0900

    feat: added url pattern

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:id>', views.index_page, name='index_page'),
+    path('<str:id>', views.index_str, name='index_str'),
 ]
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def index_page(request, id):
+    return HttpResponse('This is INT urls test. id = ' + str(id))
+
+def index_str(request, id):
+    return HttpResponse('This is STR urls test. id = ' + str(id))
