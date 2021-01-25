2fdf0431703a5d0810fb4848b2385278c37585f7
YukiarC <yuukihagino@gmail.com>
2021-01-25 23:35:41 +0900

    style: apply temp for detail

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 from django.shortcuts import render
 
@@ -25,7 +25,11 @@ def index_str(request, id):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
