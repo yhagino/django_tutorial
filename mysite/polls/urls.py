f8f40dc93189a8baa748252b181f138cc9a86ebe
YukiarC <yuukihagino@gmail.com>
2021-01-25 23:23:15 +0900

    feat: added detail, results, vote

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>', views.index_page, name='index_page'),
     path('<str:id>', views.index_str, name='index_str'),
+    path('<int:question_id>/', views.detail, name='detail'),  # ex: /polls/5/
+    path('<int:question_id>/results/', views.results, name='results'),  # ex: /polls/5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # ex: /polls/5/vote/
 ]
-
